ce45eea025e89d90a0666246927aef04a01824b1
sinkaroid <bibsindra@gmail.com>
2023-04-18 11:17:37 +0700

    test: add some testing and prototype

---
+++ src/utils/options.ts
@@ -0,0 +1,9 @@
+export default {
+  PORNHUB: "https://www.pornhub.com",
+  XNXX: "https://www.xnxx.com",
+  REDTUBE: "https://www.redtube.com",
+  XVIDEOS: "https://www.xvideos.com",
+  XHAMSTER: "https://xheve2.com",
+  YOUPORN: "https://www.youporn.com",
+  JAVHD: "https://javhd.today"
+};
