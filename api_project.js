fb9a74f6fbf2f68bbde09e1e5a4bb9e6dde3e9d7
sinkaroid <sinkaroid@users.noreply.github.com>
2023-04-19 14:38:06 +0000

    Deploying to gh-pages from @ sinkaroid/lustpress@98c7bf9a2f0e3b84402c71c515d0d9fa6accd6ad 🚀

--- api_project.js
+++ api_project.js
@@ -3,13 +3,13 @@ define({
   "url": "https://lust.scathach.id",
   "sampleUrl": "https://lust.scathach.id",
   "name": "Lustpress",
-  "version": "1.6.1-alpha",
+  "version": "1.6.2-alpha",
   "description": "RESTful and experimental API for PornHub and other porn sites, which official is lack even isn't exist.",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2023-04-18T07:47:50.610Z",
+    "time": "2023-04-19T14:38:04.871Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
